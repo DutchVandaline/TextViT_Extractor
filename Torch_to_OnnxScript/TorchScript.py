9a2db7f65a04b45677d23a08f326c56f9e7f2fce
Jason Park <142364450+DutchVandaline@users.noreply.github.com>
2025-04-13 14:36:56 +0900

    added onnx

---
+++ Torch_to_OnnxScript/TorchScript.py
@@ -0,0 +1,18 @@
+import torch
+
+from Model.KoElectraExtractor import KoElectraExtractor
+
+MAX_SEQ_LEN = 128
+BATCH_SIZE = 1
+S = 1
+
+dummy_input_ids = torch.randint(0, 100, (BATCH_SIZE, S, MAX_SEQ_LEN))
+dummy_attention_mask = torch.ones((BATCH_SIZE, S, MAX_SEQ_LEN), dtype=torch.long)
+
+model = KoElectraExtractor()
+model.load_state_dict(torch.load(r"/Checkpoints/50percent_best_kobert_f1.pt", map_location=torch.device("cpu")))
+model.eval()
+
+scripted_model = torch.jit.trace(model, (dummy_input_ids, dummy_attention_mask))
+torch.jit.save(scripted_model, r"C:\junha\Git\TextViT_Extractor\Checkpoints\koelectra_extractor.pt")
+print("모델이 성공적으로 TorchScript 형식으로 변환되어 저장되었습니다.")
---
+++ Torch_to_OnnxScript/Torch_to_Onnx.py
@@ -0,0 +1,32 @@
+import torch
+from Model.KoElectraExtractor import KoElectraExtractor
+from transformers import AutoTokenizer
+
+device = torch.device("cpu")
+model = KoElectraExtractor().to(device)
+model.load_state_dict(torch.load(r"C:\junha\Git\TextViT_Extractor\Checkpoints\50percent_best_kobert_f1.pt", map_location=device))
+model.eval()
+
+batch_size = 1
+num_sentences = 1
+seq_len = 128
+
+tokenizer = AutoTokenizer.from_pretrained("monologg/koelectra-small-discriminator")
+
+dummy_input = torch.randint(0, len(tokenizer), (batch_size, num_sentences, seq_len), dtype=torch.long)
+dummy_attention = torch.ones((batch_size, num_sentences, seq_len), dtype=torch.long)
+
+torch.onnx.export(
+    model,
+    (dummy_input, dummy_attention),         # 모델에 전달할 입력 tuple
+    "koelectra_extractor.onnx",               # 저장할 ONNX 파일명
+    input_names=["input_ids", "attention_mask"],  # 입력 이름 지정
+    output_names=["logits"],                       # 출력 이름 지정
+    dynamic_axes={
+        "input_ids": {0: "batch_size", 1: "num_sentences", 2: "seq_len"},
+        "attention_mask": {0: "batch_size", 1: "num_sentences", 2: "seq_len"},
+        "logits": {0: "batch_size", 1: "num_sentences"}
+    },
+    opset_version=11,
+)
+print("ONNX 모델이 koelectra_extractor.onnx 로 저장되었습니다.")
